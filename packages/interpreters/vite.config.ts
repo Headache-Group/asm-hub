100d70a4106e9f1e9569c45ad7ec1d1da790b351
Qmi <minh.pham1126@gmail.com>
2024-05-22 12:49:47 +0700

    chore: init eslint for package interpreters

--- packages/interpreters/vite.config.ts
+++ packages/interpreters/vite.config.ts
@@ -1,19 +1,18 @@
 /// <reference types="vitest" />
 
-import path from "path";
-import { defineConfig } from "vite";
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "index",
-      fileName: "index",
-      formats: ["cjs", "es"]
-    }
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: 'index',
+      formats: ['cjs', 'es'],
+    },
   },
   test: {
-    globals: true
+    globals: true,
   },
 });
-
